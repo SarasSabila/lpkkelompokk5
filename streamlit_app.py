ee92753b35d9ba70239e9e4f6f16cdc3d5a6b7a6
SarasSabila <sabilasaras05@gmail.com>
2025-07-03 17:02:12 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("projeklpk5")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "ayokk bisa yokkk"
 )
